1632ec5ab1b2a8573644cf318c702c90b859f583
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-06-18 08:21:39 +0800

    Server-side entry point

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+
+dotenv.config();
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
+});
+
+

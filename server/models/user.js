994a65969c5a5dfb477381ac54eb46c855970ed7
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-06-29 11:09:34 +0800

    Adjusted the user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 
 const userSchema = new mongoose.Schema({ 
@@ -33,7 +34,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    courses:{
+    course:{
         type: String,
         required: true,
     },
@@ -43,6 +44,14 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.pre('save', async function (next) {
+    
+    if (this.isModified('password')){
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+        next();
+});
 
 const User = mongoose.model('User', userSchema);
 

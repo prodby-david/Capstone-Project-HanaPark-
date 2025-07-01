2553690c64cac6cc012ca5f997426c9723cffc44
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-01 08:10:25 +0800

    Motion configurations

---
+++ client/src/lib/motionConfigs.js
@@ -0,0 +1,42 @@
+
+export const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+}
+export const buttonMotion = {
+  whileHover: {
+    scale: 1.05,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 15,
+    },
+  },
+  whileTap: {
+    scale: 0.95,
+  },
+};
+
+export const fadeUp = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } },
+}
+
+export const fadeDown = {
+  hidden: { opacity: 0, y: -20 },
+  show: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } },
+}
+export const fadeLeft = {
+  hidden: { opacity: 0, x: -20 },
+  show: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeOut" } },
+}
+
+export const fadeRight = {
+  hidden: { opacity: 0, x: 20 },
+  show: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeOut" } },
+}

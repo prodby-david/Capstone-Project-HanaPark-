c04448d6514e6e7ec1273d9aba60f83614f72572
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-01 13:20:04 +0800

    Adjusted the animation speedd

--- client/src/lib/motionConfigs.js
+++ client/src/lib/motionConfigs.js
@@ -24,19 +24,19 @@ export const buttonMotion = {
 
 export const fadeUp = {
   hidden: { opacity: 0, y: 20 },
-  show: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } },
+  show: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
 }
 
 export const fadeDown = {
   hidden: { opacity: 0, y: -20 },
-  show: { opacity: 1, y: 0, transition: { duration: 1, ease: "easeOut" } },
+  show: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
 }
 export const fadeLeft = {
   hidden: { opacity: 0, x: -20 },
-  show: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeOut" } },
+  show: { opacity: 1, x: 0, transition: { duration: 0.5, ease: "easeOut" } },
 }
 
 export const fadeRight = {
   hidden: { opacity: 0, x: 20 },
-  show: { opacity: 1, x: 0, transition: { duration: 1, ease: "easeOut" } },
+  show: { opacity: 1, x: 0, transition: { duration: 0.5, ease: "easeOut" } },
 }

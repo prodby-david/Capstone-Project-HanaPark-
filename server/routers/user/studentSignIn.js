ca8d7d6d274d2abe00d26836df6ca77a1c8ebfba
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-07 08:51:35 +0800

    Adjusted some user routes

--- server/routers/user/studentSignIn.js
+++ server/routers/user/studentSignIn.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import studentSignInController from '../../controllers/users/signin.js';
 
+
 const UserRouter = express.Router();
 
 UserRouter.post('/sign-in', studentSignInController);
+
 
 export default UserRouter;

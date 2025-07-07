33655ff8b52d5eded6bfd8748a027b3f66a125d7
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-07 08:57:23 +0800

    Adjusted cors

--- server/index.js
+++ server/index.js
@@ -5,22 +5,23 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import StudentAccountRouter from './routers/admin/studentAccount.js';
 import UserRouter from './routers/user/studentSignIn.js';
+import AdminRegistration from './routers/admin/registerAdmin.js';
+import VerifyAdmin from './routers/admin/verifyAdmin.js';
 
 
 dotenv.config();
 
 const app = express();
 
-
-
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true, 
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(StudentAccountRouter, UserRouter);
+app.use(StudentAccountRouter, UserRouter, AdminRegistration, VerifyAdmin);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);

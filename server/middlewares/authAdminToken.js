531e584b52b90c4ad2b1ce7ee1054f7b973d2721
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-24 08:22:28 +0800

    Admin access token authenticator

---
+++ server/middlewares/authAdminToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+
+const authAdminToken = (req, res, next) => {
+    
+    const admin_token = req.cookies.admin_token;
+
+    if (!admin_token) {
+        return res.status(401).json({ message: "Unauthorized access. No token provided." });
+    }
+
+    try {
+        const decoded = jwt.verify(admin_token, process.env.ADMIN_ACCESS_KEY);
+        req.user = decoded; 
+        next(); 
+    } catch (error) {
+        console.error("Token verification failed:", error);
+        return res.status(403).json({ message: "Forbidden. Invalid token." });
+    }
+};
+
+export default authAdminToken;

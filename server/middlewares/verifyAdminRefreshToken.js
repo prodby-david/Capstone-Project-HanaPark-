0ae8bf60618c52730262de9afa20c9f1cfcd3e4c
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-24 08:20:25 +0800

    Admin refresh token validator

---
+++ server/middlewares/verifyAdminRefreshToken.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+
+const VerifyAdminRefreshToken = async (req,res,next) => {
+
+    const admin_refresh_token = req.cookies.admin_refresh_token;
+
+    if(!admin_refresh_token){
+        return res.status(401).json({message: ' No refresh token provided.'});
+    }
+
+    try{
+        const decoded = jwt.verify(admin_refresh_token, process.env.ADMIN_REFRESH_KEY);
+        req.user = decoded;
+    }
+    catch(err){
+        return res.status(403).json({message: 'Invalid refresh token.'});
+    }
+
+}
+
+export default VerifyAdminRefreshToken;

f9fdd92c94e52579a675178d640d0d9f1996ff1d
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-24 08:59:48 +0800

    Added socket.io

--- server/index.js
+++ server/index.js
@@ -2,29 +2,55 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import http from 'http';
+import { Server } from 'socket.io';
 import connectDB from './config/db.js';
-import StudentAccountRouter from './routers/admin/studentAccount.js';
-import UserRouter from './routers/user/studentSignIn.js';
-import AdminRegistration from './routers/admin/registerAdmin.js';
-import VerifyAdmin from './routers/admin/verifyAdmin.js';
-import AdminSignIn from './routers/admin/adminSignin.js';
+import UserRouter from './routers/user/userRoutes.js';
+import AdminRoute from './routers/admin/adminRoutes.js';
 
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app); 
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  },
+});
+
 
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true, 
 }));
 
+io.on('connection', (socket) => {
+  console.log('A client connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(StudentAccountRouter, UserRouter, AdminRegistration, VerifyAdmin, AdminSignIn);
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
-app.listen(process.env.PORT, () => {
+
+app.use('/admin', AdminRoute);  
+app.use('/', UserRouter);       
+
+
+server.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
     connectDB();
 });

07b9291bd1cfd8347843b7e0fd55a930f96f25a7
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-07-24 08:52:07 +0800

    Admin sign in controller

---
+++ server/controllers/admin/adminSignIn.js
@@ -0,0 +1,55 @@
+import Admin from '../../models/admin.js';
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+
+const AdminSignInController = async (req,res) => {
+
+    try {
+
+        const { adminusername, adminpassword } = req.body;
+
+        const admin = await Admin.findOne({ adminusername });
+
+        if(!admin){
+            return res.status(404).json({message: "Admin username doesn't exists."})
+        }
+
+       const isAdminPasswordMatch = await bcrypt.compare(adminpassword, admin.adminpassword);
+        if(!isAdminPasswordMatch){
+            return res.status(401).json({message: "Password is incorrect. Please try again."});
+        }
+
+        const payload = {
+            userId: admin._id,
+            role: 'admin'
+        }
+
+        const admin_token = jwt.sign(payload, process.env.ADMIN_ACCESS_KEY, {expiresIn: "1h"});
+
+        const adminrefreshtoken = jwt.sign(payload, process.env.ADMIN_REFRESH_KEY, {expiresIn: '7d'})
+
+        res.cookie('admin_token', admin_token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 3600000,
+            sameSite: 'Strict'
+        });
+
+        res.cookie('admin_refresh_token', adminrefreshtoken, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 3600000,
+            sameSite: 'Strict'
+        });
+
+        res.status(200).json({message: 'Admin Sign in success.', success: true});
+
+    } 
+    catch (err) {
+        res.status(500).json({message: 'Server internal error.', success: false});
+        return;
+    }
+
+}
+
+export default AdminSignInController;

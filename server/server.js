abb6d7cc1736b50f5f80dfd5a6caaa49f6bf6a24
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-09-29 11:40:11 +0800

    Fix some bugs

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app); 
 
 const allowedOrigins = [
-  'https://capstone-project-hana-park.vercel.app', 
+  'https://hanapark-system.vercel.app', 
   'http://localhost:5173', 
 ];
 

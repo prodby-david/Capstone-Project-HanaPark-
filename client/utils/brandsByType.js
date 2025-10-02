4669149a25152448ae61cce5f829537c4d3113ae
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-02 14:53:12 +0800

    Added vehicle brand file

---
+++ client/utils/brandsByType.js
@@ -0,0 +1,13 @@
+const brandsByType = {
+  "Small Motorcycle": ["Honda", "Yamaha", "Suzuki", "Kawasaki"],
+  "Bigbike Motorcycle": ["Honda", "Yamaha", "Suzuki", "Kawasaki"],
+  "2-Wheels (400cc and above)": ["Honda", "Yamaha", "Suzuki", "Kawasaki", "Ducati", "BMW", "KTM", "Triumph"],
+  "Sedan": ["Toyota","Honda","Mitsubishi","Nissan","Hyundai","Mazda","Chevrolet","Ford"],
+  "Hatchback": ["Toyota", "Honda", "Suzuki", "Kia", "Hyundai", "Chevrolet"],
+  "SUV": ["Toyota", "Honda", "Ford", "Mitsubishi", "Nissan", "Isuzu", "Hyundai", "Chevrolet"],
+  "Pickup": ["Ford", "Isuzu", "Toyota", "Nissan", "Mitsubishi", "Mazda"],
+  "MPV": ["Toyota", "Mitsubishi", "Suzuki", "Honda", "Nissan"],
+  "Van": ["Toyota", "Nissan", "Hyundai", "Foton"],
+};
+
+export default brandsByType;

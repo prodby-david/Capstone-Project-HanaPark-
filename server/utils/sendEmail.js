4a3f81f5b6313e0407771a7f33a1d8dde2c01603
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-07 15:30:53 +0800

    feat: add a custom domain

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -8,7 +8,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendResetEmail = async (to, subject, html) => {
   try {
     await resend.emails.send({
-      from: 'Hanapark <no-reply@hanapark.vercel.app>', 
+      from: 'Hanapark <no-reply@hanapark.online>', 
       to,
       subject,
       html,

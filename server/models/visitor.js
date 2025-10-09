0d590237f33def163665db9b585d939b41d324b5
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-09 12:32:49 +0800

    refactor: refactored schema

--- server/models/visitor.js
+++ server/models/visitor.js
@@ -1,18 +1,56 @@
 import mongoose from 'mongoose'
 
 const visitorSchema = new mongoose.Schema({
+    visitorSlotId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Slot'
+    },
     visitorName: {
         type: String,
         required: true
     },
+    email:{
+        type: String,
+        required: true
+    },
+    plateNumber:{
+        type: String,
+        required: true,
+        unique: true
+    },
     contactNumber:{
         type: Number,
-        required: true
+        required: true,
+        unique: true
     },
     purposeOfVisit:{
         type: String,
         required: true
     },
+    vehicleType:{ 
+        type: String, 
+        required: true 
+    },
+    status:{ 
+        type: String, 
+        default: 'Pending' 
+    },
+    createdAt:{ 
+        type: Date, 
+        default: Date.now 
+    },
+    qrCode:{
+        type: String,
+        required: true
+    },
+    isEntryUsed: {
+        type: Boolean, 
+        default: false 
+    }, 
+    isExitUsed: {
+        type: Boolean, 
+        default: false 
+    },   
 })
 
 const Visitor = mongoose.model('Visitor', visitorSchema);

5538dc9e6f8312d93a5c4af849d92d343f305bf3
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-12 13:49:59 +0800

    feat: add feedback schema

---
+++ server/models/feedback.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+
+const feedbackSchema = mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    message:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+export default Feedback;

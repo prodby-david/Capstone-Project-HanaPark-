8e567f83ff235bca86fca61e811607776bbd50dd
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-12 19:38:59 +0800

    style: adjust padding

--- client/src/components/footers/footer.jsx
+++ client/src/components/footers/footer.jsx
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-color text-white w-full py-10">
-      {/* Main Content */}
+    <footer className="bg-color text-white w-full py-5">
+      
       <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row justify-between items-center md:items-start gap-10 px-5">
 
-        {/* About Section */}
         <div className="flex flex-col items-center md:items-start text-center md:text-left">
-          <h2 className="font-semibold text-lg text-color-3">About HanaPark</h2>
+          <h2 className="font-semibold text-lg text-white">About HanaPark</h2>
           <p className="max-w-[350px] mt-2 text-xs text-justify">
             HanaPark is a smart parking management solution designed to streamline parking systems, reduce stress, and helps you find the perfect spots every time.
           </p>
         </div>
 
-        {/* Quick Links */}
         <div className="flex flex-col items-center md:items-start text-center md:text-left">
-          <h2 className="font-semibold text-lg text-color-3">Quick Links</h2>
+          <h2 className="font-semibold text-lg text-white">Quick Links</h2>
           <ul className="mt-2 grid grid-cols-2 gap-x-5 gap-y-2 md:flex md:flex-col text-xs">
             <li><Link to='/sign-in' className="hover:text-color-3">Log in</Link></li>
             <li><Link to='/faqs' className="hover:text-color-3">FAQ's</Link></li>
@@ -25,9 +23,8 @@ const Footer = () => {
           </ul>
         </div>
 
-        {/* Contact */}
         <div className="flex flex-col items-center md:items-start text-center md:text-left text-xs">
-          <h2 className="font-semibold text-lg text-color-3 mb-2">Contact</h2>
+          <h2 className="font-semibold text-lg text-white mb-2">Contact</h2>
           <p>Email: support@hanapark.com</p>
           <p>Phone: +63 912 345 6789</p>
           <p>Location: Metro Manila, PH</p>

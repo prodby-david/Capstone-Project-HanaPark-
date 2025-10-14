a3876c880076fb310fd068d5096cd5aeac6a9092
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-14 12:43:15 +0800

    fix: fix bugs on activity logs and some ui

--- server/controllers/users/cancelReservation.js
+++ server/controllers/users/cancelReservation.js
@@ -1,35 +1,35 @@
-import Reservation from "../../models/reservation.js";
+import Reservation from "../../models/reservation.js"; 
 import Slot from "../../models/slot.js";
 
-const CancelReservation = async (req,res) => {
+const CancelReservation = async (req, res) => {
+  try {
+    const { id } = req.params;   
+    const userId = req.user?.userId;
 
-    try {
-        const { id } = req.params;   
-        const userId = req.user?.userId;
-
-        if(!userId){
-            res.status(401).json({message: 'Unauthorized access. Please log in.'});
-            return;
-        }
-
-        const reservation = await Reservation.findByIdAndUpdate(id,{status: 'Cancelled'},{new: true})
-        .populate('reservedBy', 'firstname lastname userType studentId staffId').populate('slotId', 'slotType slotCode');
-
-        if (!reservation) {
-            return res.status(404).json({ message: "Reservation not found" });
-        }
-
-        await Slot.findByIdAndUpdate(
-            reservation.slotId, 
-            {slotStatus: 'Available'}, 
-            {new: true}
-        );
-
-        res.status(200).json({ message: "Reservation cancelled successfully", reservation });
-
-    } catch (err) {
-        res.status(500).json({ message: "Error cancelling reservation", err });
+    if (!userId) {
+      return res.status(401).json({ message: 'Unauthorized access. Please log in.' });
     }
-}
+
+    const reservation = await Reservation.findByIdAndUpdate(
+      id,
+      { status: 'Cancelled' },
+      { new: true }
+    )
+    .populate('reservedBy', 'firstname lastname userType studentId staffId')
+    .populate('slotId', 'slotType slotCode');
+
+    if (!reservation) {
+      return res.status(404).json({ message: "Reservation not found" });
+    }
+
+    await Slot.findByIdAndUpdate(reservation.slotId, { slotStatus: 'Available' });
+
+    req.io.to('admins').emit('reservationCancelledByUser', reservation); 
+
+    res.status(200).json({ message: "Reservation cancelled successfully", reservation });
+  } catch (err) {
+    res.status(500).json({ message: "Error cancelling reservation", err });
+  }
+};
 
 export default CancelReservation;

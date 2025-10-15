cc4302b10d3e584707c6c9f33357505422b327dd
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-15 14:04:12 +0800

    fix: fix the real time update when user cancels

--- server/controllers/users/cancelReservation.js
+++ server/controllers/users/cancelReservation.js
@@ -1,6 +1,6 @@
 import Reservation from "../../models/reservation.js"; 
 import Slot from "../../models/slot.js";
-import Activity from '../../models/activitylogs.js'
+import Activity from '../../models/activitylog.js'
 
 const CancelReservation = async (req, res) => {
   try {
@@ -24,6 +24,8 @@ const CancelReservation = async (req, res) => {
 
     await Slot.findByIdAndUpdate(reservation.slotId, { slotStatus: 'Available' });
 
+    console.log('ReservedBy user:', reservation.reservedBy);
+
     const newActivity = new Activity({
       reservationId: reservation._id,
       reservedBy: reservation.reservedBy._id,
@@ -34,7 +36,7 @@ const CancelReservation = async (req, res) => {
     await newActivity.save();
 
     const populatedActivity = await Activity.findById(newActivity._id)
-    .populate('reservedBy', 'firstname lastname userType studentId').lean();
+    .populate('reservedBy', 'firstname lastname userType studentId staffId').lean();
 
     req.io.to('admins').emit('reservationCancelledByUser', populatedActivity);
  

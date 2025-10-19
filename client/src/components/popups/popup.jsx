66138f1cc79759dd9b07bc9efc72a15acd1183f0
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-19 13:51:41 +0800

    fix: change swal alert to custom pop up

--- client/src/components/popups/popup.jsx
+++ client/src/components/popups/popup.jsx
@@ -27,7 +27,7 @@ const CustomPopup = ({
     <AnimatePresence>
       {show && (
         <motion.div
-          className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/40 backdrop-blur-sm"
+          className="fixed inset-0 z-[99] flex items-center justify-center bg-black/40 backdrop-blur-sm"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}

58aa030beca5592d584c04c5f216a90b9fd4aa82
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-19 12:57:40 +0800

    fix: fix the refresh token

--- server/controllers/admin/adminRefreshToken.js
+++ server/controllers/admin/adminRefreshToken.js
@@ -12,7 +12,7 @@ const AdminRefreshTokenController = async (req,res) => {
 
     res.cookie('admin_token', newAdminAccessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         maxAge: 60 * 60 * 1000,
         sameSite: 'None'
     });

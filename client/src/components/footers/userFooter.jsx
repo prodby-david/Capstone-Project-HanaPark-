da391039e20d02f1dac02e720b472441a18fb296
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-25 10:55:17 +0800

    fix: change the footer

--- client/src/components/footers/userFooter.jsx
+++ client/src/components/footers/userFooter.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const UserFooter = () => {
   return (
     <footer className="bg-white text-center text-color-2 text-sm md:text-md py-5 mt-3 w-full border-t border-gray-200">
-      &copy; {new Date().getFullYear()} HanaPark. All rights reserved.
+      <p className="text-xs mt-1">
+          Developed by <span className="font-semibold">John David Gaspar</span>,{" "}
+          <span className="font-semibold">Elijah Santos</span>,{" "}
+          <span className="font-semibold">Jan Symon Clemente</span>, and{" "}
+          <span className="font-semibold">Renz Barbecho</span>
+        </p>
     </footer>
   )
 }

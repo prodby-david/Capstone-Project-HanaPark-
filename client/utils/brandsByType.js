9cd445e3a2232df261271b42c3b543194b72bce6
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-25 11:25:23 +0800

    fix: add vehicle brands

--- client/utils/brandsByType.js
+++ client/utils/brandsByType.js
@@ -1,7 +1,55 @@
 const brandsByType = {
-    "2-Wheels (399cc below)": ["Honda", "Yamaha", "Suzuki", "Kawasaki", "Others"],
-    "2-Wheels (400cc up)": ["Harley-Davidson", "BMW", "Ducati", "Others"],
-    "4-Wheels": ["Toyota", "Honda", "Ford", "Nissan", "Hyundai", "Chevrolet", "Kia", "Mazda", "Mitsubishi", "Subaru", "Others"]
+  "2-Wheels (399cc below)": [
+    "Honda",
+    "Yamaha",
+    "Suzuki",
+    "Kawasaki",
+    "Kymco",
+    "SYM",
+    "Vespa",
+    "MotorStar",
+    "Rusi",
+    "Skygo",
+    "Bristol",
+    "Others"
+  ],
+  "2-Wheels (400cc up)": [
+    "Harley-Davidson",
+    "BMW",
+    "Ducati",
+    "Kawasaki",
+    "Triumph",
+    "Honda Big Bikes",
+    "Yamaha Big Bikes",
+    "KTM",
+    "CFMoto",
+    "Royal Enfield",
+    "Bristol",
+    "Benelli",
+    "Others"
+  ],
+  "4-Wheels": [
+    "Toyota",
+    "Honda",
+    "Ford",
+    "Nissan",
+    "Hyundai",
+    "Chevrolet",
+    "Kia",
+    "Mazda",
+    "Mitsubishi",
+    "Subaru",
+    "Isuzu",
+    "Foton",
+    "Changan",
+    "Geely",
+    "MG",
+    "BYD",
+    "GAC",
+    "Suzuki",
+    "Peugeot",
+    "Others"
+  ]
 };
 
 export default brandsByType;

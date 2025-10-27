9be45f18d0138d219f4abd969f4261c0fe2c89db
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-27 23:29:46 +0800

    feat: userlog schema

---
+++ server/models/userlog.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userLogSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  action: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UserLog = mongoose.model("User_Log", userLogSchema);
+
+export default UserLog;

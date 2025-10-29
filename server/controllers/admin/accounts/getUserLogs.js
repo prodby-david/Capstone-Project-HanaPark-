26c280532ec2d35d146b62d701fb2f149a11aa64
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-10-29 16:38:18 +0800

    feat: get user logs controller

---
+++ server/controllers/admin/accounts/getUserLogs.js
@@ -0,0 +1,17 @@
+import UserLog from "../../../models/userLog.js";
+
+
+
+const getUserLogs = async (req, res) => {
+  try {
+    const logs = await UserLog.find()
+      .populate("userId", "firstname lastname userType") 
+      .sort({ createdAt: -1 }); 
+
+    res.status(200).json(logs);
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
+
+export default getUserLogs;
